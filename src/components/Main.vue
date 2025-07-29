7411e0684b88ba881f431275e969a3bbc4331d42
Aleksa <aleksamlsvc12@gmail.com>
2025-07-29 11:19:25 +0200

    scroll on main div fixed

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,7 +3,11 @@
 </script>
 
 <template>
-  <div class="p-[5%] grid grid-cols-1 md:grid-cols-2 justify-items-center items-center">
+  <div class="p-[5%] grid grid-cols-1 md:grid-cols-2 justify-items-center gap-6 md:gap-10 h-full overflow-y-auto">
+    <Profile />
+    <Profile />
+    <Profile />
+    <Profile />
     <Profile />
     <Profile />
   </div>
