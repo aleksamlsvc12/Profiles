8df9a99f5dbfc5aa9d78761fc56f355d56358d58
Aleksa <aleksamlsvc12@gmail.com>
2025-07-29 10:47:55 +0200

    profile box sizing

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,3 +1,9 @@
 <template>
-  <div class="p-[5%]">Main</div>
+  <div class="p-[5%] flex flex-col justify-around items-center md:flex-row">
+    <div class="w-full h-[45%] md:w-[40%] md:h-full rounded-[5%] shadow-[0_0_10px_1px_rgba(0,0,0,0.4)]"></div>
+    <div class="w-full h-[45%] md:w-[40%] md:h-full rounded-[5%] shadow-[0_0_10px_1px_rgba(0,0,0,0.4)]"></div>
+  </div>
 </template>
+
+
+
