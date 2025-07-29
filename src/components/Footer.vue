3963a437425ee5a22d5268038e17db1bc3b81f22
Aleksa <aleksamlsvc12@gmail.com>
2025-07-29 20:08:57 +0200

    footer copyright text changed

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="bg-blue-500 flex justify-center items-center">
-    <span class="text-white text-xs">Profiles made by Aleksa Milošević</span>
+    <span class="text-white text-xs">&copy; 2025 Profiles - Created by Aleksa Milosevic</span>
   </div>
 </template>
