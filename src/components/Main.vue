ad08b537e7c805af98727f8a7838f999582458eb
Aleksa <aleksamlsvc12@gmail.com>
2025-07-29 20:21:45 +0200

    worked on responsive design for bigger screens. needs more fixing

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="p-[5%] grid grid-cols-1 md:grid-cols-2 justify-items-center gap-6 md:gap-10 h-full overflow-y-auto">
+  <div class="p-[5%] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-6 md:gap-10 h-full overflow-y-auto">
     <Profile />
     <Profile />
     <Profile />
