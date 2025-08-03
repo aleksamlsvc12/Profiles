ae55e05848a4743538942cf110ff5dc2121871fc
Aleksa <aleksamlsvc12@gmail.com>
2025-08-03 23:43:27 +0200

    added wallpapers, hardcoded to show trough an array

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,20 +1,32 @@
 <script setup>
-  import Profile from "@/components/Profile.vue";
+import Profile from "@/components/Profile.vue";
 
-  import profiles from "@/assets/data/profiles.json";
+import profiles from "@/assets/data/profiles.json";
 
-  import p0 from "@/assets/img/p0.jpg";
-  import p1 from "@/assets/img/p1.jpg";
-  import p2 from "@/assets/img/p2.jpg";
-  import p3 from "@/assets/img/p3.jpg";
-  import p4 from "@/assets/img/p4.jpg";
-  import p5 from "@/assets/img/p5.jpg";
-  import p6 from "@/assets/img/p6.jpg";
-  import p7 from "@/assets/img/p7.jpg";
-  import p8 from "@/assets/img/p8.jpg";
-  import p9 from "@/assets/img/p9.jpg";
+import p0 from "@/assets/img/p0.jpg";
+import p1 from "@/assets/img/p1.jpg";
+import p2 from "@/assets/img/p2.jpg";
+import p3 from "@/assets/img/p3.jpg";
+import p4 from "@/assets/img/p4.jpg";
+import p5 from "@/assets/img/p5.jpg";
+import p6 from "@/assets/img/p6.jpg";
+import p7 from "@/assets/img/p7.jpg";
+import p8 from "@/assets/img/p8.jpg";
+import p9 from "@/assets/img/p9.jpg";
 
-  const images = [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9];
+import wp0 from "@/assets/img/wp0.jpg";
+import wp1 from "@/assets/img/wp1.jpg";
+import wp2 from "@/assets/img/wp2.jpg";
+import wp3 from "@/assets/img/wp3.jpg";
+import wp4 from "@/assets/img/wp4.jpg";
+import wp5 from "@/assets/img/wp5.jpg";
+import wp6 from "@/assets/img/wp6.jpg";
+import wp7 from "@/assets/img/wp7.jpg";
+import wp8 from "@/assets/img/wp8.jpg";
+import wp9 from "@/assets/img/wp9.jpg";
+
+const profilePics = [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9];
+const wallpapers = [wp0, wp1, wp2, wp3, wp4, wp5, wp6, wp7, wp8, wp9];
 </script>
 
 <template>
@@ -32,7 +44,8 @@
         :hourlyRate="profile.hourlyRate"
         :about="profile.about"
         :location="profile.location"
-        :image = "images[profile.id]"
+        :profilePic="profilePics[profile.id]"
+        :wallpaper="wallpapers[profile.id]"
       />
     </div>
   </div>
